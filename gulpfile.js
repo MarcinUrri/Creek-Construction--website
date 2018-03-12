0843146210567c9e36326ee5e6904766f90289eb
Marcin <marcinurri@gmail.com>
2018-03-12 16:26:01 +0100

    simple changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,24 @@
 const gulp = require('gulp');
 const browserSync = require('browser-sync');
-const sass = require('gulp-sass');
 const sourcemaps = require('gulp-sourcemaps');
 const cleanCSS = require('gulp-clean-css');
 const uglify = require('gulp-uglify');
 const concat = require ('gulp-concat');
-const babel = require('gulp-babel');
 const imagemin = require('gulp-imagemin');
 
 gulp.task('reload', function(){
   browserSync.reload();
 });
 
-gulp.task('serve', ['sass'], function(){
+gulp.task('serve', function(){
   browserSync({
     server: 'dist'
   });
   gulp.watch('src/*.html', ['reload']);
-  gulp.watch('src/scss/**/*.scss', ['sass']);
   gulp.watch('src/js/**/*.js', ['js']);
   gulp.watch('src/css/**/*.css', ['css']);
 });
 
-gulp.task('sass', function(){
-  return gulp.src('src/scss/**/*.scss')
-  .pipe(sourcemaps.init())
-  .pipe(sass().on('error', sass.logError))
-  .pipe(sourcemaps.write())
-  .pipe(gulp.dest('src/css'))
-  .pipe(browserSync.stream());
-});
 
 gulp.task('css', function(){
     return gulp.src('src/css/**/*.css')
